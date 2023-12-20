f4f11f28113790815ca931a2c4819a240885fab0
syg4 <syg4@pitt.edu>
2023-12-20 17:55:58 -0500

    Fixed and simplified
    Normalized vectors and forced them to be orthogonal using svd. Simplified some of the code where I calculated unnecessary displacement vectors.

--- definevectors.m
+++ definevectors.m
@@ -4,13 +4,9 @@ a1 = atom1;
 a2 = atom2;
 a3 = atom3;
 
-v1 = [n(a2).x - n(a1).x;
-           n(a2).y - n(a1).y;
-           n(a2).z - n(a1).z];
+v1 = [n(a2).x; n(a2).y; n(a2).z];
 
-v2 = [n(a3).x - n(a1).x;
-           n(a3).y - n(a1).y;
-           n(a3).z - n(a1).z];
+v2 = [n(a3).x; n(a3).y - n(a1).y; n(a3).z - n(a1).z];
 
 if abs(dot(v1,v2)) > 0.01
     warning('Atoms chosen do not form orthogonal vectors')
@@ -19,11 +15,13 @@ else
     v3 = cross(v1,v2);
 end  
 
+v1 = v1/sqrt(sumsqr(v1));
+v2 = v2/sqrt(sumsqr(v2));
+v3 = v3/sqrt(sumsqr(v3));
+
 for ii = 1:length(n)
     vc(ii).Atom = n(ii).Atom;
-    vc(ii).xyz = [n(ii).x - n(a1).x;
-                  n(ii).y - n(a1).y;
-                  n(ii).z - n(a1).z];
+    vc(ii).xyz = [n(ii).x; n(ii).y; n(ii).z];
 end
 vectorcoordinates = vc;
 end
--- transformbasis.m
+++ transformbasis.m
@@ -1,18 +1,13 @@
 function transformedcoordinates = transformbasis(coordinates,v1,v2,v3)
 c = coordinates;
 
-v_x = [1; 0; 0];
-v_y = [0; 1; 0];
-v_z = [0; 0; 1];
-
 v = [v1,v2,v3];
-v_new = [v_x,v_y,v_z];
-
-A = v_new/v;
+[u,s,w] = svd(v);
+v_corr = u*w';
 
 for ii = 1:length(c)
     t(ii).Atom = c(ii).Atom;
-    t_xyz = A*c(ii).xyz;
+    t_xyz = v_corr'*c(ii).xyz;
     t(ii).x = t_xyz(1);
     t(ii).y = t_xyz(2);
     t(ii).z = t_xyz(3);
